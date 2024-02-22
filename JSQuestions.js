b8b6411bbfd4dfaa78098cdff0af74666bb3c229
Zain Sheikh <zainsheikh61506@gmail.com>
2024-02-22 10:12:06 -0500

    commit post hw- I HATE ARROW FUNCTIONS GRAHHHHHH

--- JSQuestions.js
+++ JSQuestions.js
@@ -11,3 +11,24 @@ var studentgrade = Math.floor(Math.random() * 100);
 
 var check = studentgrade < 70 ? "F" : studentgrade > 70 && studentgrade < 80 ? "C" : studentgrade > 80 && studentgrade < 90 ?  "B" : "A";
 console.log(studentgrade + " -> " + check);
+
+function multiplication(number){
+    return number * number;
+}
+
+console.log(randomnumbah + " * " + studentgrade + " = " + multiplication(randomnumbah, studentgrade));
+
+multiplyrandom = (Math.random() * 100, Math.random() * 100) => {
+    multiplication(multiplyrandom);
+}
+
+multiplication(Math.random() * 100, Math.random() * 100) => number * number;
+
+
+arryay = new Array.from(length = 15), String() => "stringex";
+console.log(arryay.size)
+
+
+
+
+
