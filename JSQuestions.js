40805273471616cae234b4402132e11c57845f89
Zain Sheikh <zainsheikh61506@gmail.com>
2024-02-22 11:25:57 -0500

    Post-Class making arrow things work

--- JSQuestions.js
+++ JSQuestions.js
@@ -12,21 +12,35 @@ var studentgrade = Math.floor(Math.random() * 100);
 var check = studentgrade < 70 ? "F" : studentgrade > 70 && studentgrade < 80 ? "C" : studentgrade > 80 && studentgrade < 90 ?  "B" : "A";
 console.log(studentgrade + " -> " + check);
 
-function multiplication(number){
-    return number * number;
+
+// console.log(randomnumbah + " * " + studentgrade + " = " + multiplication(randomnumbah, studentgrade));
+
+
+function multiplication(number, number2){
+    return number * number2;
+}
+
+multiplyrandom = (Number, Number2) => {
+    console.log(Number , " ",Number2)
+   console.log( multiplication(Number, Number2));
 }
 
 console.log(randomnumbah + " * " + studentgrade + " = " + multiplication(randomnumbah, studentgrade));
 
-multiplyrandom = (Math.random() * 100, Math.random() * 100) => {
-    multiplication(multiplyrandom);
+multiplyrandom(Math.random() * 100, Math.random() * 100);
+
+//const array1 = new Array;
+const array1 = [];
+
+for(var x = 0; x < 15; x++){
+    array1[x] = "stringex";
+    console.log(array1[x]);
 }
+var z = Math.random()*14
+console.log(array1.length, array1.lastIndexOf("stringex"), array1[z]);
 
-multiplication(Math.random() * 100, Math.random() * 100) => number * number;
-
-
-arryay = new Array.from(length = 15), String() => "stringex";
-console.log(arryay.size)
+//array1 = Array.from({length: 15}, () => "stringex");
+// console.log(array1.size)
 
 
 
